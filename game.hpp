3680aead80c472cfe99aa8d6561a8214360b3369
bicSmera42 <belluccidamiano@gmail.com>
2016-10-23 00:49:08 +0200

    Update game.hpp

--- game.hpp
+++ game.hpp
@@ -46,7 +46,67 @@ protected:
     int id; //identificatore giocatore
     int lp; //punti vita giocatore
     int maxlp;
-    Inventory inventario; //inventario associato al goicatore
+    Inventory* inventario; //inventario associato al goicatore
+public:
+    Player();
+    Player(int identificatore);
+    int showId();
+    int life();
+    void lifeFix(int value);
+    int useObject(int slotInventary);
+    void TakeObject(Object oggetto);
+    int maxHp();
+    Inventory* showInventory();
+};
+
+struct Node{
+    Player player;
+    struct Node* next;
+};
+typedef struct Node node;
+
+class Queue{
+protected:
+    node* q; //puntatore al primo nodo della lista
+public:
+    Queue();
+    void enqueue (Player giocatore); //input id del player
+    void dequeue(int idPlayer); //torna la testa della lista in quanto potrebbe cambiare se il primo della lista viene eliminato
+    bool isEmpty();
+    Queue* returnListPointer();
+    Node* returnHead();
+};
+
+struct databaseObject{
+    Object* oggetto[6];
+};
+typedef struct databaseObject databaseObject;
+
+class Manage{
+protected:
+    int n;
+    Queue *l; //puntatore all'oggetto lista
+    databaseObject* database;
+    int nPlayers;
+    int nRounds;
+    Object defObj;
+    //map
+public:
+    Manage();
+    void builtQueue();
+    void fetchDatabaseObjects(Object oggettoDelGioco);
+    void spawnMonsterOrObject(Player* giocatore);
+    void dropObject(Player* giocatore);
+    bool databaseEmpty();
+    void setRounds(int n);
+    void setPlayers(int n);
+    void startGame();
+    Queue* returnList();
+    void assignDefaultObject();
+    void setDefaultObject(Object oggetto);
+};
+#endif /* GAME_HPP_ */
+
 public:
     Player(int identificatore);
     int showId();
