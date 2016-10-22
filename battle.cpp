8fd4a14bd1c8d50c17841631d5410b29a63d2a61
bicSmera42 <belluccidamiano@gmail.com>
2016-10-23 00:44:07 +0200

    Update battle.cpp

--- battle.cpp
+++ battle.cpp
@@ -160,3 +160,117 @@ int battle::CalculateDamage(int Damage){  //ritorna 0 se sono entrambi vivi 1 se
 	if (p->life()<=0) status=2;
 	return status;
 }
+
+			HealthBar(p->life(), p->maxHp(), romero->LifePoints(), romero->MaxLifePoints(),p->showId());
+		}
+		else if(spiderman!=NULL){
+			HealthBar(p->life(), p->maxHp(), spiderman->LifePoints(), spiderman->MaxLifePoints(),p->showId());
+		}
+	if (stato==1){
+		WinScreen();
+	}
+	if (stato==2){
+		LoseScreen();
+	}
+}
+int battle::IOManager(){ //ritorna il danno del oggetto che si è scelto
+	cout<<endl;
+	cout<<"Scegli l'oggetto che vuoi usare per colpire il "<<MonsterName<<":"<<endl;
+	int select=1;
+	for (int i=0; i<5; i++){
+		if (p->showInventory()->slotIsFull(i)){
+			cout<<select<<") "<< p->showInventory()->getName(i)<< "(dmg "<<p->showInventory()->AccessObjectFromInventory(i)<<")"<<endl;
+			select++;
+		}
+	}
+	select--;
+	int counter=select;
+	do{
+		cin>>select;
+		if ((select<1) || (select>counter)) cout<<"Devi selezionare un oggetto valido!"<<endl;
+	}while((select<1) || (select>counter));
+	counter=1;
+	bool exit=false;
+	int i=0;
+	bool ispotion=false;
+	while (!exit){
+		if ((p->showInventory()->slotIsFull(i)) && (select==counter)){
+			if (p->showInventory()->AccessObjectFromInventory(i)==0){
+				p->lifeFix(-potionHP);
+				cout<<"usi la Pozione che ti restituisce "<<potionHP<<" HP!"<<endl;
+				p->showInventory()->deleteObject(i);
+				ispotion=true;
+			}
+			exit=true;
+		}
+		else if (p->showInventory()->slotIsFull(i)) counter++;
+		i++;
+	}
+	i--;
+	if(!ispotion) return p->showInventory()->AccessObjectFromInventory(i);
+    	else return 0;
+}
+void battle::WinScreen(){
+	cout<<"			CONGRATULAZIONI HAI BATTUTO IL "<<MonsterName<<endl;
+    manage->dropObject(p);
+}
+void battle::LoseScreen(){
+	cout<<"			SEI MORTO"<<endl;
+}
+void battle::HealthBar(int currenthpPlayer,int maxhpPlayer, int currenthpMonster,int maxhpMonster, int playerID){ //20 barrette
+	cout<<"Player "<<playerID<<"                              "<<MonsterName<<endl;
+	cout<<" ____________________"<<"                 "<<" ____________________"<<endl;
+	cout<<'|';
+	int i=1;
+	while(i<=20){
+		if(((20*currenthpPlayer)/maxhpPlayer)>=i)cout<<"█";
+		else cout<<' ';
+		i++;
+	}
+	cout<<"|                |";
+	i=1;
+	while(i<=20){
+		if(((20*currenthpMonster)/maxhpMonster)>=i)cout<<"█";
+		else cout<<' ';
+		i++;
+	}
+	cout<<'|'<<endl;
+	cout<<" --------------------"<<"                 "<<" --------------------"<<endl;
+	cout<<"      "<<currenthpPlayer<<"/"<<maxhpPlayer<<"                                "<<currenthpMonster<<"/"<<maxhpMonster<<endl;
+}
+int battle::CalculateDamage(int Damage){  //ritorna 0 se sono entrambi vivi 1 se il mostro è morto 2 se il giocatore è morto
+	int status=0;
+	int beforebattle=p->life();
+		if (dracula!=NULL){
+			p->lifeFix(dracula->getDmg());
+			dracula->GotHit(Damage);
+			cout<<"fai "<<Damage<<" di danno al "<<dracula->GetName()<<" e ne subisci "<<beforebattle-(p->life())<<endl<<endl;
+			if (dracula->LifePoints()<=0){
+				delete dracula;
+				dracula=NULL;
+				status=1;
+			}
+		}
+		else if (spiderman!=NULL){
+			p->lifeFix(spiderman->getDmg(p));
+			spiderman->GotHit(Damage);
+			cout<<"fai "<<Damage<<" di danno al "<<spiderman->GetName()<<" e ne subisci "<<beforebattle-(p->life())<<endl<<endl;
+			if (spiderman->LifePoints()<=0){
+				delete spiderman;
+				spiderman=NULL;
+				status=1;
+			}
+		}
+		else if(romero!=NULL){
+			p->lifeFix(romero->getDmg());
+			romero->GotHit(Damage);
+			cout<<"fai "<<Damage<<" di danno al "<<romero->GetName()<<" e ne subisci "<<beforebattle-(p->life())<<endl<<endl;
+			if (romero->LifePoints()<=0){
+				delete romero;
+				romero=NULL;
+				status=1;
+			}
+		}
+	if (p->life()<=0) status=2;
+	return status;
+}
