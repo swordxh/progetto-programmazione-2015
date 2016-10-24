86bb71974a18f894d6caf09440a2d4e1a09a5b6d
bicSmera42 <belluccidamiano@gmail.com>
2016-10-24 14:38:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@
 #include "boss.hpp"
 
 int main(){
-    Object spada=Object("spada",30);
+    Object spada=Object("spada",50);
+    Object mitragliatrice=Object("mitragliatrice",70);
+
     Object piccone=Object("piccone",10);
     Manage g=Manage();
     
     g.setDefaultObject(piccone); //carico oggetto di base
     g.fetchDatabaseObjects(spada);
-    g.fetchDatabaseObjects(spada);
+    g.fetchDatabaseObjects(mitragliatrice);
     
     g.startGame();
     return 0;
