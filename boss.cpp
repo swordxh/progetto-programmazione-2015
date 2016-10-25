b2d1a85e43994a788a943c46b2df3d3e17c21fb9
ArkantosH6 <mirkidonewowowo@gmail.com>
2016-10-25 22:35:27 +0200

    Implementazione lista nella battaglia finale (DA FIXARE)

--- boss.cpp
+++ boss.cpp
@@ -1,13 +1,12 @@
 #include "boss.hpp"
 using namespace std;
 
-boss::boss(){
+boss::boss(int *nPlayers){
 	strcpy(name,"Drago\0"); // nome
-	MaxHp= (n * 100) * z; // "n" = numero giocatori, "z" = fattore che moltiplica vita
+	MaxHp= (*nPlayers * 100) * z; // "n" = numero giocatori, "z" = fattore che moltiplica vita
 	Hp=MaxHp;
 	damagest = 20; // attacco standard
 	damage1 = 10; // attacco che colpisce tutti i giocatori
-	damage2 = 100; // attacco mortale
 }
 char *boss::GetName(){
 	return name;
@@ -27,18 +26,30 @@ int boss::MaxLifePoints(){
 int boss::speciale1(){
 	return damage1;
 } // il boss usa l'attacco che colpisce tutti i giocatori
-int boss::speciale2(){
-	return damage2;
-} // il boss usa l'attacco mortale
-finale::finale(Player *p_curr){
-    p = p_curr; // prende il giocatore dalla lista (DA IMPLEMENTARE)
+
+finale::finale(Queue *p_curr, int n){
+    l = p_curr;
+    head = NULL; //prendono la lista
 	b = NULL;
 	MonsterName[0]='\0';
 	k = 1; // Iniziano i turni
+	att = 1; // inizio dal primo giocatore
+	m = 0;
+	scelto = 0;
+	p = NULL; // inizio dalla testa
+	q = NULL;
+	database=NULL;
+	Node* scorr = head;; //con scorr vedo l'id più alto, così assegno n
+	nPlayers = n;
+	delete scorr;
+}
+Queue* finale::returnList(){
+    return l;
 }
 void finale::battleManager(){
+    head = this -> returnList() -> returnHead();
     if (b == NULL) {
-        b = new boss();
+        b = new boss(&nPlayers);
         strcpy(MonsterName, b->GetName());
     }// inizializzo il boss
 	int stato=0; // Inizia la battaglia
@@ -47,14 +58,25 @@ void finale::battleManager(){
 	cout << "Nessun sinistro rumore di passi..." << endl;
 	cout << "Come se tutto cio' fosse la quiete prima della tempesta..." <<endl;
 	cout<<endl<<endl<<endl<<"Il "<<MonsterName<<" ti attacca, inizi cosi a difenderti."<<endl;
+	p = this -> returnList() -> returnHead(); // mi metto nella testa della lista
 	while (stato==0){
-		if (b!=NULL){
-			HealthBar(p->life(), 100, b->LifePoints(), b->MaxLifePoints(),p->showId()); // gli do gli HP attuali\massimi dei giocatori e del boss, aggiungendo l'ID del giocatore
-		} // se il boss è vivo, vado in HealthBar
-		stato=CalculateDamage(IOManager());
+        p=this->returnList()->returnHead();
+        for(int j = 0; j < nPlayers; j++){
+            q = &p -> player;
+            if (b!=NULL){
+                HealthBar(q->life(), q->maxHp(), b->LifePoints(), b->MaxLifePoints(), q->showId()); // gli do gli HP attuali\massimi dei giocatori e del boss, aggiungendo l'ID del giocatore
+            } // se il boss è vivo, vado in HealthBar
+            stato=CalculateDamage(IOManager());
+            p = p -> next; //vado al giocatore successivo
+            if (p == head) att = -1; //Sono ritornato alla testa? se sì, do a "att" il valore -1
+            else att++; //non sono ritornato alla testa, ho trovato il giocatore successivo
+        } // attaccano i giocatori
+		stato=CalculateDamage(att); //attacca il boss
+		att = 1; // così permetto ai giocatori di attaccare nel turno successivo
+        k++; // turno finito, passo a quello successivo
 	}// finchè "stato" = 0 (ovvero i giocatori e il boss sono vivi) io sto dentro il while
 	if (b!=NULL){
-				HealthBar(p->life(), 100, b->LifePoints(), b->MaxLifePoints(),p->showId());
+				HealthBar(p->player.life(), p->player.maxHp(), b->LifePoints(), b->MaxLifePoints(), p->player.showId());
 			}// in questo caso i giocatori sono morti (sono fuori dal while)
 	if (stato==1){
 		WinScreen(MonsterName); // vittoria
@@ -65,33 +87,40 @@ void finale::battleManager(){
 }
 int finale::IOManager(){ //ritorna il danno del oggetto che si è scelto
 	cout<<endl;
-	cout<<"Scegli l'oggetto che vuoi usare per colpire il "<<MonsterName<<":";
-	int select=1; // select elenca gli oggetti selezionabili
-	for (int i=0; i<5; i++){
-		if (p->showInventory()->slotIsFull(i)){
-			cout<<select<<") "<< p->showInventory()->getName(i)<< "(dmg "<<p->showInventory()->AccessObjectFromInventory(i)<<")"<<endl;
+	cout<<"Scegli l'oggetto che vuoi usare per colpire il "<<MonsterName<<":"<<endl;
+	int select=1;
+	for (int m = 0; m < 5; m++){
+		if (p->player.showInventory()->slotIsFull(m)){
+			cout<<select<<") "<< p->player.showInventory()->getName(m)<< "(dmg "<<p->player.showInventory()->AccessObjectFromInventory(m)<<")"<<endl;
 			select++;
-		} //lo slot non deve essere vuoto
+		}
 	}
-	int counter=select; // counter indica l'oggetto col select più alto (in poche parole, l'ultimo)
+	select--;
+	int counter=select;
 	do{
-	cin>>select;
-	}while((select<1) || (select>counter)); // seleziono un oggetto, il valore deve essere compreso tra 1 e counter
-	counter=1; //
-	bool exit=false; //exit è una guardia del while
+		cin>>select;
+		if ((select<1) || (select>counter)) cout<<"Devi selezionare un oggetto valido!"<<endl;
+	}while((select<1) || (select>counter));
+	counter=1;
+	bool exit=false;
 	int i=0;
+	bool ispotion=false;
 	while (!exit){
-		if ((p->showInventory()->slotIsFull(i)) && (select==counter)){
-			if (p->showInventory()->AccessObjectFromInventory(i)==0){
-				p->lifeFix(-20);
-				p->showInventory()->deleteObject(i);
-			}// caso della pozione: a differenza degli altri oggetti (armi), questo viene eliminato, dando 20 HP al giocatore (la pozione ha il campo damage = 0)
-			exit=true; //posso uscire dal while, ho usato l'oggetto
-		}// se l'oggetto è quello selezionato, lo uso
-		else if (p->showInventory()->slotIsFull(i)) counter++;
+		if ((p->player.showInventory()->slotIsFull(i)) && (select==counter)){
+			if (p->player.showInventory()->AccessObjectFromInventory(i)==0){
+				p->player.lifeFix(-potionHP);
+				cout<<"usi la Pozione che ti restituisce "<<potionHP<<" HP!"<<endl;
+				p->player.showInventory()->deleteObject(i);
+				ispotion=true;
+			}
+			exit=true;
+		}
+		else if (p->player.showInventory()->slotIsFull(i)) counter++;
 		i++;
 	}
-	return p->showInventory()->AccessObjectFromInventory(i--);
+	i--;
+	if(!ispotion) return p->player.showInventory()->AccessObjectFromInventory(i);
+    	else return 0;
 }
 void finale::WinScreen(char MonsterName[]){
 	cout<<"					CONGRATULAZIONI HAI BATTUTO IL "<<MonsterName<<endl;
@@ -100,8 +129,9 @@ void finale::LoseScreen(){
 	cout<<"					SEI MORTO"<<endl;
 }
 void finale::HealthBar(int currenthpPlayer,int maxhpPlayer, int currenthpMonster,int maxhpMonster, int playerID){ //20 barrette
+    int g;
     cout <<endl<< "                                     " << MonsterName << endl<<" ";
-    for(int g=1; g<=78;g++)cout<<"-";
+    for(g=1; g<=78;g++)cout<<"-";
     cout<<endl<<'|';
 	int i=1;
 	while(i<=78){
@@ -117,7 +147,7 @@ void finale::HealthBar(int currenthpPlayer,int maxhpPlayer, int currenthpMonster
         i++;
     }// calcola le | in base agli HP attuali tramite una percentuale
     cout<<'|' << endl<<" "; // le parentesi le ho messe solo per delimitare la barra della salute
-    for(int g=1; g<=78;g++)cout<<"-";
+    for(g=1; g<=78;g++)cout<<"-";
     cout<<endl<<endl<<endl;
 	cout << "     _______M''''''M____                     ____M''''''''''''M________" << endl;
     cout << " __/      .        . __/\\__            _____/\\___  .8M.. .. D:   +\\_   \\ ___ " << endl;
@@ -147,37 +177,89 @@ void finale::HealthBar(int currenthpPlayer,int maxhpPlayer, int currenthpMonster
         else cout << "     ";
         i++;
 	}// la V indica il giocatore attuale
-	i = 1;
-	while(i <= n){
-        cout << "   " << p -> showId();
-		if(i < n)cout<<"    ";
-		i++;
+	Node* scorr = this->returnList()->returnHead();
+	Player* q1 = &scorr -> player;
+	for(i = 1; i <= nPlayers; i++){
+        q1 = &scorr -> player;
+        cout << "   " << scorr -> player.showId();
+		if(i < nPlayers)cout<<"    ";
+		scorr = scorr -> next;
 	}
 	cout<<endl;
-	i = 1;
-	while(i <= n){
-        cout << p -> life() << "/100";
-		if(i < n)cout<<" ";
-		i++;
+	scorr = this->returnList()->returnHead();
+	for(i = 1; i <= nPlayers; i++){
+        q1 = &scorr -> player;
+        if ((q1 -> life()) >= 100) cout << q1 -> life() << "/100";
+        else cout << " " << q1 -> life() << "/100";
+		if(i < nPlayers)cout<<" ";
+        scorr = scorr -> next;
 	}
 	cout<<endl;
+	delete scorr;
+	delete q1;
 }
 int finale::CalculateDamage(int Damage){  //ritorna 0 se sono entrambi vivi 1 se il mostro è morto 2 se il giocatore è morto
 	int status=0;
 	if (b!=NULL){
-        if ((k % 2) == 0) p->lifeFix(b->speciale1()); // ogni due turni usa l'attacco che colpisce tutti i giocatori (DEVO AVERE LA LISTA PER SCORRERLA)
-        else if ((k % 3) == 0) p->lifeFix(b->speciale2()); // ogni tre turni usa l'attacco mortale (DEVO AVERE LA LISTA PER ACCEDERE AL GIOCATORE CON MENO VITA)
-		else p->lifeFix(b->getDmg());// attacco standard (DEVO AVERE LA LISTA PER ATTACCARE UN QUALSIASI GIOCATORE)
-		b->GotHit(Damage); // il boss subisce danni dal giocatore (DEVO AVERE LA LISTA PER PERMETTERE L'ATTACCO A TUTTI I GIOCATORI)
+        if (att != (-1)) b->GotHit(Damage); // il boss subisce danni dal giocatore
+        else{
+            Node* scorr = this->returnList()->returnHead();
+            Player *q1 = &scorr -> player;
+            if ((k % 2) == 0){
+                for (int i=1; i<=nPlayers; i++){
+                    q1 = &scorr -> player;
+                    q1 -> lifeFix(b->speciale1()); // usa l'attacco che colpisce tutti i giocatori
+                    scorr = scorr -> next; //va al nodo successivo
+                    if (scorr == head) att = 1; //sono ritornato alla testa, il boss ha finito
+                }
+            } //ogni due turni usa l'attacco che colpisce tutti i giocatori
+            else if ((k % 3) == 0) {
+                    att = q1 -> life();
+                    scelto = q1 -> showId(); //potrebbe essere il primo giocatore quello ad avere meno vita
+                    for (int i=1; i<=nPlayers; i++){
+                        q1 = &scorr -> player;
+                        if ((q1 -> life()) < att) {
+                                att = q1 -> life();
+                                scelto = q1 -> showId();
+                        } // ho trovato un giocatore che ha meno vita
+                        scorr = scorr -> next; //va al nodo successivo
+                        if (scorr == head) att = -1; //Sono ritornato alla testa? se sì, do a "att" il valore -1
+                    } //cerca il giocatore con meno vita
+                    att = (-1);
+                    scorr = this->returnList()->returnHead();
+                    while (att == (-1)){
+                        q1 = &scorr -> player;
+                        if ((q1 -> showId()) == scelto){
+                            q1 -> lifeFix(scorr -> player.life()); // ogni tre turni usa l'attacco mortale
+                            att = 1;
+                        }
+                        scorr = scorr -> next; //va al nodo successivo
+                    }
+                    scelto = 0;
+            }
+            else {
+                    srand(time(0));
+                    scelto = (rand() % nPlayers) + 1; //sceglie casualmente uno degli "n" giocatori della lista
+                    while (att == (-1)){
+                        q1 = &scorr -> player;
+                        if ((q1 -> showId()) == scelto){
+                            q1 -> lifeFix(b->getDmg());// attacco standard
+                            att = 1;
+                        }
+                        scorr = scorr -> next; //va al nodo successivo
+                    }
+                    scelto = 0;
+            }
+            delete scorr;
+            delete q1;
+        } // stavolta è il boss che attacca
 		if (b->LifePoints()<=0){
 			delete b;
 			b=NULL;
 			status=1;
 		} // il boss è morto
 	}
-	if (p->life()<=0) status=2; // morte di 1 o più giocatori (DEVO AVERE LA LISTA PER CONTROLLARE LA SALUTE DI TUTTI I GIOCATORI)
+	if (p->player.life()<=0) status=2; // morte di 1 o più giocatori (DEVO AVERE LA LISTA PER CONTROLLARE LA SALUTE DI TUTTI I GIOCATORI)
 	// L'ISTRUZIONE E' DA COMPLETARE: devo distinguere l'eliminazione di uno o più giocatori e il game over (morte di tutti i giocatori)
-    k++; // turno finito, passo al successivo
 	return status;
 }
-
--- boss.hpp
+++ boss.hpp
@@ -4,39 +4,48 @@
 #include <unistd.h>
 #include "game.hpp"
 
-const int n = 1, k = 1, z = 1.5, att = 1; // n indica il numero dei giocatori, k aumenta con l'avanzare dei turni, z Ã‹ il moltiplicatore per la vita del boss, att il giocatore attuale
+const int z = 2; // n indica il numero dei giocatori, k aumenta con l'avanzare dei turni, z è il moltiplicatore per la vita del boss, att il giocatore attuale
 
 class boss{
 protected:
 	int MaxHp; //HP massimi
 	int Hp; // HP attuali
 	char name[10];// nome
-	int damagest, damage1, damage2; // damagest Ã‹ l'attacco standard, damage1 quello che attacca tutti i giocatori, damage2 quello che uccide istantaneamente il giocatore con meno vita
+	int damagest, damage1; // damagest è l'attacco standard, damage1 quello che attacca tutti i giocatori, damage2 quello che uccide istantaneamente il giocatore con meno vita
 public:
-	boss(); // costruttore
+	boss(int *nPlayers); // costruttore
 	char *GetName(); // da il nome del boss
 	int getDmg(); // metodo col quale il boss usa l'attacco standard
 	void GotHit(int dmg); // il boss subisce i danni dei giocatori
 	int LifePoints(); // ritorna gli HP attuali
 	int MaxLifePoints(); // ritorna gli HP massimi
     int speciale1(); // L'attacco speciale che colpisce tutti i giocatori
-    int speciale2(); // L'attacco mortale
 };
 
 class finale{
 protected:
-    Player *p;
+    Queue *l; // l serve per scorrere la lista, head è un riferimento per i turni
+    Node* p; //p = giocatore attuale; head = testa
+    Node* head;
     boss *b;
-    int k; // per ciclicitâ€¡ attacchi
+    Player *q;
+    int k; // per ciclicità attacchi
+    int nPlayers; // numero dei giocatori
     char MonsterName[10]; // nome boss
+    databaseObject* database; //mi serve per usare l'inventario
+    int att; //indica se attacca il boss (-1) o i giocatori (il loro ID)
+    int scelto; //indica quale giocatore attaccare nell'attacco standard e in quello mortale
+    const int potionHP=30;
+    int m;
 public:
-	finale(Player *p_curr); // costruttore
+	finale(Queue *p_curr, int n); // costruttore
 	void battleManager(); // metodo che gestisce le basi della battaglia
 	int IOManager(); // permette al giocatore di accedere all'inventario
 	int CalculateDamage(int Damage);
 	void WinScreen(char MonsterName[]); // vittoria
 	void LoseScreen(); // perdita di un giocatore (DA IMPLEMENTARE IL GAME OVER DI TUTTI I GIOCATORI, SERVE LA LISTA)
 	void HealthBar(int currenthpPlayer,int maxhpPlayer, int currenthpMonster,int maxhpMonster, int playerID); // gestione della grafica e delle barre della salute del giocatore e del boss
+	Queue* returnList();
 };
 
 #endif // BOSS_H_INCLUDED
