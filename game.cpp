a2d56b1a94a8a7b0e16af9502d65f9759527bfd9
bicSmera42 <belluccidamiano@gmail.com>
2016-10-26 21:41:39 +0200

    bug fix

--- game.cpp
+++ game.cpp
@@ -148,48 +148,51 @@ void Queue::enqueue (Player giocatore){
 
 void Queue::dequeue(int idPlayer){
     if (q!=NULL) {
-        if ((q->player).showId()==idPlayer && q->next==q) { //caso in cui c'è solo primo elemento (OK)
+        if ((q->player).showId()==idPlayer && q->next==q){ //caso in cui c'è solo primo elemento
             Node* app=q;
             q=NULL;
             delete app;
+            app=NULL;
 
         }
         else{
             if ((q->player).showId()==idPlayer) { //caso in cui elimino elemento in testa in una lista di elementi
-
                 Node* track=q; //faccio puntare ultimo elemento al nuovo primo elemento visto che sto eliminando testa
                 while (track->next!=q) {
                     track=track->next;
                 }
+                
                 track->next=q->next;
                 track=NULL;
-                delete track;
-
-                Node* app; //elimino elemento di testa //perchè non funziona senza questa riga di comando?
+                
+                Node* app; //elimino elemento di testa
                 app=q;
                 q=q->next;
                 delete app;
+                app=NULL;
                 }
             else{ //caso generale eliminazione nodo
                 Node* p=new Node;
                 p=q;
                 bool flag=0;
 
-                while ((p->next->player).showId()!=idPlayer && p->next->next!=q) {
+                while ((p->next->player).showId()!=idPlayer && p->next->next!=q) {//inizio a controllare dall'elemento dopo la testa visto che quest'ultima è già stata controllata nei casi prima. Caso in cui ho due elementi: non entro nel while
                     p=p->next;
                 }
 
-                if ((p->next->player).showId()==idPlayer) {
+                if ((p->next->player).showId()==idPlayer) { //devo controllare che non sia finito nel caso in cui non c'è elemento cercato e abbia solo due nodi
                     Node* app=new Node;
-                    p->next=p->next->next;
                     app=p->next;
-                    //delete app;
+                    p->next=p->next->next;
+                    delete app;
+                    app=NULL;
                 }
             }
 
         }
     }
 }
+
 Node* Queue::returnHead(){
     return q;
 }
