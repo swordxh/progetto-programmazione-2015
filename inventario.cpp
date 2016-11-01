5fbc151b8dfe7697656201ba3b5bffe4f7156960
bicSmera42 <belluccidamiano@gmail.com>
2016-11-01 21:02:29 +0100

    Create inventario.cpp

---
+++ inventario.cpp
@@ -0,0 +1,37 @@
+#include "inventario.hpp"
+using namespace std;
+
+Inventory::Inventory(){
+    int i=0;
+    for (i=0;i<5;i++){
+        oggetto[i]=NULL;
+    }
+}
+
+bool Inventory::slotIsFull(int slotInventario){ //controlla se uno specifico slot dell'inventario è pieno
+    if (oggetto[slotInventario]!=NULL) {
+        return 1;
+    }
+    else return 0;
+}
+
+int Inventory::AccessObjectFromInventory (int slotInventario){
+    return oggetto[slotInventario]->Damage();
+}
+void Inventory::insertObject(Object oggettoDaInserire, int slotInventario){
+    oggetto[slotInventario]=new Object(oggettoDaInserire.showNameObject(),oggettoDaInserire.Damage());
+    *oggetto[slotInventario]=oggettoDaInserire;
+}
+
+void Inventory::deleteObject(int slotInventario){
+    Object* app=oggetto[slotInventario];
+    delete app;
+    oggetto[slotInventario]=NULL;
+}
+char* Inventory::getName(int slotInventario){
+    return oggetto[slotInventario]->showNameObject();
+}
+
+
+
+
