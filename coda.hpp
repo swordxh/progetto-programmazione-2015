6b56ac1eaca3927ea4aa8a84c5e1ee0aa81e5602
bicSmera42 <belluccidamiano@gmail.com>
2016-11-01 21:03:39 +0100

    Create coda.hpp

---
+++ coda.hpp
@@ -0,0 +1,26 @@
+#ifndef coda_hpp
+#define coda_hpp
+
+#include "player.hpp"
+#include <stdio.h>
+
+using namespace std;
+
+struct Node{
+    Player player;
+    struct Node* next;
+};
+
+class Queue{
+protected:
+    Node* q; //puntatore al primo nodo della lista
+public:
+    Queue();
+    void enqueue (Player giocatore);
+    void dequeue(int idPlayer);
+    bool isEmpty();
+    Node* Head();
+};
+
+
+#endif /* coda_hpp */
