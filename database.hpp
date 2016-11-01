0cda485083b7cacccf72fa776abde9d893463004
bicSmera42 <belluccidamiano@gmail.com>
2016-11-01 21:01:48 +0100

    Create database.hpp

---
+++ database.hpp
@@ -0,0 +1,19 @@
+#ifndef database_hpp
+#define database_hpp
+#include "oggetto.hpp"
+
+#include <stdio.h>
+using namespace std;
+
+class databaseObject{
+protected:
+    Object* oggetto[6];
+public:
+    databaseObject();
+    Object* slotDatabase(int slot);
+    void addObjectToDatabase(int slot, Object oggetto);
+    bool databaseEmpty();
+};
+
+
+#endif /* database_hpp */
