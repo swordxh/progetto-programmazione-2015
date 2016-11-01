c219f30376f3bd662251b236569962cd66a0dbbb
bicSmera42 <belluccidamiano@gmail.com>
2016-11-01 21:02:04 +0100

    Update database.cpp

--- database.cpp
+++ database.cpp
@@ -1,1 +1,41 @@
+#include "database.hpp"
 
+databaseObject::databaseObject(){
+    int i=0;
+    for(i=0;i<6;i++){
+        oggetto[i]=NULL;
+    }
+}
+
+Object* databaseObject::slotDatabase(int slot){
+    if (oggetto[slot]==NULL) {
+        return NULL;
+    }
+    else return oggetto[slot];
+}
+
+void databaseObject::addObjectToDatabase(int slot, Object oggettoDelGioco){
+    if (oggetto[slot]==NULL) {
+        oggetto[slot]=new Object(oggettoDelGioco.showNameObject(),oggettoDelGioco.Damage());
+    }
+    else{
+        delete oggetto[slot];
+        oggetto[slot]=new Object(oggettoDelGioco.showNameObject(),oggettoDelGioco.Damage());
+    }
+}
+
+bool databaseObject::databaseEmpty(){
+    int i=0;
+    bool flag=false;
+    
+    while(i<6 && flag==0) {
+        if (this->slotDatabase(i)!=NULL){
+            flag=true;
+        }
+    }
+    
+    if (flag) {
+        return false;
+    }
+    else return true;
+}
